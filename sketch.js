dbb468746444053f868cb917de5c42148d1316fb
Omo Ajibola <88599328+omo776@users.noreply.github.com>
2023-04-03 23:13:12 -0400

    Toy files

---
+++ sketch.js
@@ -0,0 +1,20 @@
+let shapeColor;
+
+function setup() {
+  createCanvas(700, 500);
+  
+  shapeColor = color(255,200,200);
+}
+
+function draw() {
+  background(96, 100, 200)
+  text("Click the circle to change color", 30, 20)
+  noStroke();
+  fill(shapeColor);
+  ellipse(width/2, height/2, 180,180);
+  
+}
+
+function mousePressed(){
+  shapeColor = color(random(255), random(255), random(255) );
+}
